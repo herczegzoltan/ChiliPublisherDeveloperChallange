c0e8091b5cbf94e194b4433d756d4de84c3be572
Zoltan Herczeg <zoltan.herczeg>
2020-02-29 15:21:49 +0100

    UI element added

--- ChiliPublisherDeveloperChallange/MainWindow.xaml.cs
+++ ChiliPublisherDeveloperChallange/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ChiliPublisherDeveloperChallange
         {
             InitializeComponent();
         }
+
+        private void ReadXMLFile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
