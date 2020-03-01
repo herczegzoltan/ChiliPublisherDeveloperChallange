49250a03a6f1a2a61f522892e8a0b112f1d75bde
Zoltan Herczeg <zoltan.herczeg>
2020-03-01 19:17:16 +0100

    Adding buttons (Draw preview, Save) Adding Drawing helper classes Testing to draw customized rectangle

---
+++ ChiliPublisherDeveloperChallange/Infrastructure/DrawingHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace ChiliPublisherDeveloperChallange.Infrastructure
+{
+    public  class DrawingHelper
+    {
+        public static BitmapImage ConvertBitmapImageToBitmap(Bitmap src)
+        {
+            MemoryStream ms = new MemoryStream();
+            ((Bitmap)src).Save(ms, ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
+        }
+
+        public static Bitmap DrawRectangle(Bitmap bm,int x, int y, int width, int height)
+        {
+            using (Graphics gr = Graphics.FromImage(bm))
+            {
+                Rectangle rect = new Rectangle(x, y, width, height);
+                using (Pen thick_pen = new Pen(Color.Black, 1))
+                {
+                    gr.DrawRectangle(thick_pen, rect);
+                }
+            }
+
+            return bm;
+        }
+
+        public static void SaveFile()
+        {
+
+        }
+    }
+}
--- ChiliPublisherDeveloperChallange/MainWindow.xaml.cs
+++ ChiliPublisherDeveloperChallange/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using ChiliPublisherDeveloperChallange.Infrastructure;
 using ChiliPublisherDeveloperChallange.Model;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,9 +27,18 @@ namespace ChiliPublisherDeveloperChallange
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Bitmap bm = null;
+
         public MainWindow()
         {
             InitializeComponent();
+            bm = new Bitmap(500, 500);
+            using (Graphics graph = Graphics.FromImage(bm))
+            {
+                System.Drawing.Rectangle ImageSize = new System.Drawing.Rectangle(0, 0, 500, 500);
+                graph.FillRectangle(System.Drawing.Brushes.White, ImageSize);
+            }
         }
 
         private void ReadXMLFile_Click(object sender, RoutedEventArgs e)
@@ -33,38 +46,53 @@ namespace ChiliPublisherDeveloperChallange
             RootObject item = new RootObject(); 
             item = XmlProcessor.GetXmlRootObjectCommands(@"C:\Projects\BeerPack.xml");
 
-
-
             foreach (var c in item.Panels.PanelsItem.AttachedPanelsItems.AttachedPanelsItem)
             {
-
-                
                 foreach (var d in c.AttachedPanelsItems.AttachedPanelsItem)
                 {
-                    
                     foreach (var k in d.AttachedPanelsItems.AttachedPanelsItem)
                     {
-
                         foreach (var ein in k.AttachedPanelsItems.AttachedPanelsItem)
                         {
                             MessageBox.Show(ein.PanelId);
-
                         }
 
                     }
-
                 }
+            }
+        }
+     
+        private void CreatePreview_Click(object sender, RoutedEventArgs e)
+        {
+            //Bitmap bm = new Bitmap(500, 500);
+            bm = DrawingHelper.DrawRectangle(bm,10, 10,200,200);
 
-                //foreach (var a in c.PanelsItem.AttachedPanels)
-                //{
-                //    MessageBox.Show(a.PanelsItem.PanelId);
+            RenderedPreview.Source = DrawingHelper.ConvertBitmapImageToBitmap(bm);
 
-                //}
-                //foreach (var v in c.AttachedPanels)
-                //{
-                //    MessageBox.Show(v.PanelsItem);
+        }
 
-                //}
+        private void SavePreview_Click(object sender, RoutedEventArgs e)
+        {
+            bm = DrawingHelper.DrawRectangle(bm, 10, 10, 200, 200);
+
+            Graphics g = Graphics.FromImage(bm);
+            g.Flush();
+            bm.Save("file.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            g.Dispose();
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "JPeg Image|*.jpg";
+            saveFileDialog1.Title = "Save an Image File";
+            saveFileDialog1.ShowDialog();
+
+            if (saveFileDialog1.FileName != "")
+            {
+                FileStream fs = (FileStream)saveFileDialog1.OpenFile();
+
+                bm.Save(fs,ImageFormat.Jpeg);
+                fs.Close();
+
             }
         }
     }
